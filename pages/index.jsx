ad550c11c66c9cb5c9a02dffbedf195c1dd6963a
AZMAIN ARNOB <ornobazmain@gmail.com>
2025-09-09 14:05:38 +0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,10 +8,10 @@ import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary/60 h-full">
+    <div className="bg-primary/60 min-h-screen relative">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto px-4">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
@@ -52,9 +52,10 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-      {/* image */}
-      <div className="w-[1280px] h-full absolute right-0 bottom-0">
-        {/* bg img */}
+
+      {/* image & avatar */}
+      <div className="absolute inset-0">
+        {/* bg image */}
         <div
           role="img"
           className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
@@ -71,7 +72,7 @@ const Home = () => {
           animate="show"
           exit="hidden"
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+          className="absolute bottom-0 right-0 lg:right-[8%] w-48 h-48 sm:w-56 sm:h-56 md:w-72 md:h-72 xl:w-[737px] xl:h-[678px] mx-auto lg:mx-0"
         >
           <Avatar />
         </motion.div>
@@ -81,4 +82,5 @@ const Home = () => {
 };
 
 export default Home;
+
 
